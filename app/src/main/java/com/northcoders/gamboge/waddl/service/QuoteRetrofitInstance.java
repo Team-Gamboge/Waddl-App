a7fc9a202c13bbc9c4c3a6cf95577c6a24ec8f7c
Trntybth <trinitybooth89@gmail.com>
2024-10-02 12:31:41 +0100

    feat: added QuoteRetrofitInstance class with functionality to access zenquotes.io

---
+++ app/src/main/java/com/northcoders/gamboge/waddl/service/QuoteRetrofitInstance.java
@@ -0,0 +1,30 @@
+package com.northcoders.gamboge.waddl.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class QuoteRetrofitInstance {
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "https://zenquotes.io/api/today";
+
+
+    public static TaskApiService getService() {
+        if (retrofit == null) {
+            // this allows us to log the JSON body of the HTTP request
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit.create(TaskApiService.class);
+    }
+
+}
